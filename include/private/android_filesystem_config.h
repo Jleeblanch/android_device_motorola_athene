fc3bc402248c4191f4298c1af3e568cf79107342
Ajay Panicker <apanicke@google.com>
2016-09-20 11:52:14 -0700

    athene: Remove net_bt_stack group
    * Replace it with bluetooth
    
    Bug: 31549206
    Change-Id: I667963e5f9fd1a5dc9ad74378b318e3b782e6883
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -109,7 +109,6 @@
 #define AID_NET_ADMIN     3005  /* can configure interfaces and routing tables. */
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
-#define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
 #define AID_READPROC     3009  /* can read/write /dev/diag */
 #define AID_WAKELOCK           3010 /* can read/write /dev/socket/imsrtp */
 #define AID_SENSORS       3011 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
@@ -242,7 +241,6 @@ static const struct android_id_info android_ids[] = {
     { "net_bw_stats",  AID_NET_BW_STATS, },
     { "ims",           AID_IMS, },
     { "net_bw_acct",   AID_NET_BW_ACCT, },
-    { "net_bt_stack",  AID_NET_BT_STACK, },
     { "readproc", AID_READPROC, },
     { "wakelock", AID_WAKELOCK, },
 
