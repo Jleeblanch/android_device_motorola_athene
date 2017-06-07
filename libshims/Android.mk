cfe8760cff0f0f441d7cc8ed8e2e479f0205675e
Alberto97 <albertop2197@gmail.com>
2017-06-07 09:53:30 +0200

    athene: provide ASensor_getHandle for libmdmcutback
    Change-Id: I76a648bacf9c587b9022e8e8ed109846ea838ab6

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -25,3 +25,10 @@ LOCAL_SRC_FILES := rild_socket.c
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sensor.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
