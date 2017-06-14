070177aa602586e02dbda0ee34d2ad63f18b64bc
Vinay Verma <vvinay@codeaurora.org>
2017-06-14 14:35:18 +0530

    athene: vndk: Add vndk-sp
    Add vndk sp libraries to system lib.
    
    Change-Id: Ia5b0087506571a929169736dfe108e7ff3f6d40f
    
    Install sp vndk libs in system
    
    Change-Id: I4cbbf9d0e6fd615c0f23c8be06cc93e8df2069f1
    
    Update vndk-sp build mechanism and library set.
    
    This change is to add some more libraries needed to
    be present in the vndk-sp library set. Also, move towards
    a better the mechanism to build vndk-sp.
    
    Change-Id: Iac86c8ece08c2dae94fce6e94deb670aa742e3dc

--- device.mk
+++ device.mk
@@ -326,6 +326,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
