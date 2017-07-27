cda820e85409b90266f07acfa5ed1f1722884b5d
Karthik Gopalan <gkarth@codeaurora.org>
2017-07-27 16:27:21 +0530

    athene: perf: Rename core_ctl system prop
    Rename core_ctl system properties.
    
    Change-Id: I8bd8cff34e2e53d8014e81223ab27172ccf7e4a3

--- system_prop.mk
+++ system_prop.mk
@@ -94,8 +94,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.core_ctl_min_cpu=2 \
-    ro.core_ctl_max_cpu=4 \
+    ro.vendor.qti.core_ctl_min_cpu=2 \
+    ro.vendor.qti.core_ctl_max_cpu=4 \
     ro.vendor.extension_library=libqti-perfd-client.so
 
 # Play Store
