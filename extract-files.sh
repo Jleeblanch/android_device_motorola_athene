dd8bbb80726be21e9c3ce6cae3c0ff9563642492
Bruno Martins <bgcngm@gmail.com>
2017-08-16 15:55:23 +0000

    athene: Default to sanitize vendor folder during extraction
    * This was always the default behaviour and doesn't require
      any additional argument in order to make use of pinning feature
      (pinned blobs are only considered when vendor folder is removed)
    
    Change-Id: I34ff7678bd6800a2c593e9fe128cc151c59967da
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -33,8 +33,8 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-# default to not sanitizing the vendor folder before extraction
-clean_vendor=false
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -45,7 +45,7 @@ while [ "$1" != "" ]; do
                                 SECTION=$1
                                 clean_vendor=false
                                 ;;
-        -c | --clean-vendor )   clean_vendor=true
+        -n | --no-cleanup )     CLEAN_VENDOR=false
                                 ;;
     esac
     shift
@@ -56,12 +56,12 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEAN_VENDOR"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEAN_VENDOR"
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
 fi
 
