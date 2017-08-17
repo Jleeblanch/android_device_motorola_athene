aad730267c9f574ad9c7c423ae4b1669b5b1b6af
Luca Stefani <luca020400@lineageos.org>
2017-08-17 19:28:48 +0200

    athene: Complete the rebrand
    * We are LineageOS
    
    Change-Id: I0d8d573f9e0032110ae3a0f54ebe11a877b83137
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,9 +26,9 @@ INITIAL_COPYRIGHT_YEAR=2016
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -36,7 +36,7 @@ fi
 . "$HELPER"
 
 # Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
 # Copyright headers and guards
 write_headers
@@ -46,4 +46,3 @@ write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt
 
 # We are done!
 write_footers
-
