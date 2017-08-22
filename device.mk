685409aaea9eae889a9d47d807a97730aca54a49
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:08:39 +0200

    athene: Build Biometrics HIDL HAL interface
    * Also remove fingerprint service
    
    Change-Id: I110363310ace22dce661f710e6f98993d7ac2c18

--- device.mk
+++ device.mk
@@ -117,6 +117,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Fingerprint sensor
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
