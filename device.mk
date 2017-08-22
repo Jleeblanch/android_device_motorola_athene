4a135e45c7668e0a754fb7b06fcb73a86b093aa9
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    athene: Build Vibrator HIDL HAL interface
    Change-Id: I3353060afd4f7b63ebd94a3a46d40cb7e651d8b5

--- device.mk
+++ device.mk
@@ -304,6 +304,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-athene.conf:system/etc/thermal-engine-athene.conf
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
