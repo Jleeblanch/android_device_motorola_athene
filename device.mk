a681ff1eebd0f0535b37101a8772366ae116449d
Luca Stefani <luca020400@lineageos.org>
2017-08-24 01:43:10 +0800

    athene: Build DRM HIDL HAL interface
    Change-Id: I10d5a1cd3a09be132f56fe59a2667236df3cb594

--- device.mk
+++ device.mk
@@ -108,7 +108,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # FM
 PRODUCT_PACKAGES += \
