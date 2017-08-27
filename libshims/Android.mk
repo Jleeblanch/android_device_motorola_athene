dd5675c6cf8ed5cf52fa30bb9d5451c187ed5953
Alberto97 <albertop2197@gmail.com>
2017-08-27 23:38:19 +0200

    athene: Fix ril shim build
    Change-Id: If6edd604219cdba94a366351467315607d9fc3f2

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -28,7 +28,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sensor.cpp
-LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_SHARED_LIBRARIES := libsensor
 LOCAL_MODULE := libshim_ril
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
