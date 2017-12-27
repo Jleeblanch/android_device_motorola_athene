8af7cae799c93cd9d3887d65ef36bb34bdd70c9d
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-27 23:39:54 +0200

    athene: Add VNDK HALs
    Change-Id: I1723d5531674b1d84c711a655f6476544bb0a956

--- device.mk
+++ device.mk
@@ -357,6 +357,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-athene.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
