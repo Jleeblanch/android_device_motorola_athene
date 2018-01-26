dc4d1d286facd78d09338b761c0b5ac031d891fd
LuK1337 <priv.luk@gmail.com>
2018-01-26 15:51:18 +0000

    athene: Convert audio_effects.conf to XML
    * Converted using : https://github.com/luk1337/aeffects-conf2xml
    
    Change-Id: Ida4dabb206f9c9767cc17f5d002d64bca092ed94

--- device.mk
+++ device.mk
@@ -56,7 +56,7 @@ PRODUCT_COPY_FILES +=  \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
