ddd643c81a288ff0bdcd579cb276814b985fd942
Harikrishnan Hariharan <hahariha@codeaurora.org>
2018-02-20 22:06:37 +0530

    athene: Start loc_launcher as gps user
    Start loc_launcher as gps user and set uid/gid of loc_launcher
    to gps and enable SETUID and SETGID caps for loc_launcher, so
    that we can start loc_launcher as gps user instead of root and
    still have capabilities for SETUID and SETGID. Also remove not
    required permissions groups for loc_launcher.
    
    Change-Id: Ie9f1506874b3c3148f9170c8d5db9afd1e70025c
    CRs-Fixed: 2192881

--- config.fs
+++ config.fs
@@ -61,6 +61,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
+[vendor/bin/loc_launcher]
+mode: 0755
+user: AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
 [vendor/bin/slim_daemon]
 mode: 0755
 user:  AID_GPS
