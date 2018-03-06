77ff1fc1d165d9f6dbab07af30a7ebe5367e37eb
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2018-03-05 22:36:04 -0500

    athene: Enable dexpreopt for boot and system server
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,16 @@ TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
 BOARD_HARDWARE_CLASS += \
     $(DEVICE_PATH)/lineagehw
 
+# Enable dexpreopt to speed up boot time
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 # Display
 BOARD_USES_ADRENO := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
