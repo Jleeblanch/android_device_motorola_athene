02bafd6f728205393e4ab53d7743ba7094c8a2f8
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    athene: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -359,6 +359,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-athene.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
