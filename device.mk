35585db11eeca0cb1d1db897313e58d90141c644
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-08-04 11:12:15 -0400

    athene: Import msm_irqbalance configs
    * Copy configs to vendor
    * From athene Oreo OPJ28.111-22-1
    
    Change-Id: Ibabb4dbef2533615eeec741e8fc18fac49b981b5

--- device.mk
+++ device.mk
@@ -187,6 +187,11 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
